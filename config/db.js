0161f3ea23236458f2c3b4b72989fb00c364f37e
piyush181105 <piyushjaiswal1805@gmail.com>
2026-04-30 19:09:34 +0530

    Organize project into backend and frontend directories

--- config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://piyushjaiswal1805_db_user:6NTePTIbztC8Oi7a@cluster0.gozj1n6.mongodb.net/Ledgr ")
-        .then(() => console.log("Connected to MongoDB"));
-}
