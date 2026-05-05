0e0e052b0f3177427d87f60e33f39fa5ddc384dc
piyush181105 <piyushjaiswal1805@gmail.com>
2026-05-05 16:55:36 +0530

    Explicitly define backend as serverless function

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from './route/userRoute.js';
 import incomeRouter from "./route/incomeRoute.js";
 import expenseRouter from "./route/expenseRoute.js";
 import dashboardRouter from "./route/dashboardRoute.js";
+import app from "../api/index.js";
 
 
 const app = express();
